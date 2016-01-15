3ec77a893aba4a9a96759229de1c388927b2ac40
skohar <skohar@users.noreply.github.com>
2016-01-15 13:02:18 +0900

    Add avahi

--- basic-utils.sh
+++ basic-utils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y make wget curl tmux git htop
+sudo apt-get install -y make wget curl tmux git htop avahi-discover avahi-utils avahi-daemon avahi-autoipd
