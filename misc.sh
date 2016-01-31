b6f215e002566916e0354cd1877cbc702000d2ae
skohar <skohar@users.noreply.github.com>
2016-01-31 17:11:14 +0900

    Add linuxbrew

---
+++ misc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
