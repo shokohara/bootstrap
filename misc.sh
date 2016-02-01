6e343a40330bb329ad22980403405a49d0bfb04a
skohar <skohar@users.noreply.github.com>
2016-02-01 14:57:26 +0900

    halfway

--- misc.sh
+++ misc.sh
@@ -3,3 +3,12 @@
 sudo apt-get update
 sudo apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
+
+mkdir $HOME/bin
+curl -s https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt > ~/bin/sbt && chmod 0755 ~/bin/sbt
+
+sudo apt-get install -y openjdk-8-jdk
+
+wget https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip
+sudo apt-get install -y unzip
+unzip packer_0.8.6_linux_amd64.zip -d ~/bin
