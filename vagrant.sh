429867d2638a15d5b1b496219e1349a4824fda4b
skohar <skohar@users.noreply.github.com>
2016-02-01 14:54:51 +0900

    Add vagrant and virtualbox

--- vagrant.sh
+++ vagrant.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo apt-get install -y virtualbox vagrant
+wget http://download.virtualbox.org/virtualbox/5.0.14/virtualbox-5.0_5.0.14-105127~Ubuntu~wily_amd64.deb
+sudo dpkg -i virtualbox-5.0_5.0.14-105127~Ubuntu~wily_amd64.deb
+
+#wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+#sudo dpkg -i vagrant_1.8.1_x86_64.deb
