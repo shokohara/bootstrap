0c88a4a89d1a75087ef9040af95b071d5596091c
skohar <skohar@users.noreply.github.com>
2016-02-22 12:06:10 +0900

    Enable clipboard

--- vim.sh
+++ vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y vim
+sudo apt-get -y install vim vim-gtk vim-athena vim-gnome
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
