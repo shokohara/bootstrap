1b3a60003156edfdbb51ae129066daff953e61e8
skohar <skohar@users.noreply.github.com>
2016-02-22 14:04:15 +0900

    Add power-line fonts

--- misc.sh
+++ misc.sh
@@ -39,3 +39,13 @@ sudo apt-get install -y inotify-tools
 
 sudo apt-get install -y mdadm
 sudo apt-get install -y xsel
+pip install --user powerline-status
+pip install --user git+git://github.com/powerline/powerline
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts
+mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts/
+mkdir -p ~/.config/fontconfig/conf.d
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
