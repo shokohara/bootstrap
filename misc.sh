c33890203f9f7f41d0c3d753877b6636bd09e365
shok4a <shok4a@users.noreply.github.com>
2016-05-26 15:20:54 +0900

    Remove font

--- misc.sh
+++ misc.sh
@@ -42,10 +42,3 @@ sudo apt-get install -y xsel
 pip install --user powerline-status
 pip install --user git+git://github.com/powerline/powerline
 
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.fonts
-mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts/
-mkdir -p ~/.config/fontconfig/conf.d
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
