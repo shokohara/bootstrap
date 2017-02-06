541e4f8896a88c578a6111e84ec465f0f63a146c
Sho Kohara <shokohara@users.noreply.github.com>
2017-02-06 19:41:35 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y make wget curl tmux git htop avahi-discover avahi-utils avahi-daemon avahi-autoipd
 
-sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker $(id -u -n)
