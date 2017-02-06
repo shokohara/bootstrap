ad9744fc31b07218c1f5ae12574cb6a3791767c1
Sho Kohara <shokohara@users.noreply.github.com>
2017-02-06 19:46:28 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,21 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-curl -fsSL https://get.docker.com/ | sh
+sudo apt-get update
+sudo apt-get install -y curl \
+  linux-image-extra-$(uname -r) \
+  linux-image-extra-virtual
+sudo apt-get install -y apt-transport-https \
+  ca-certificates
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository \
+     "deb https://apt.dockerproject.org/repo/ \
+     ubuntu-$(lsb_release -cs) \
+     main"
+sudo apt-get update -y
+sudo apt-get -y install docker-engine
 sudo usermod -aG docker $(id -u -n)
 
 sudo apt-get update
