d7dfde612f83ef7f202303dd330b237c9cc9d9f1
Sho Kohara <shokohara@users.noreply.github.com>
2017-02-10 11:18:11 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,9 @@
 set -e
 sudo apt-get update
 sudo apt-get install -y git curl
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
 git clone https://github.com/shokohara/bootstrap-ubuntu.git $HOME/dotfiles
 $HOME/dotfiles/deploy.sh
