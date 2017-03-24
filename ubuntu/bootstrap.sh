6f8a7dc585d5fa805d28fbb1884482057ca4b84e
Sho Kohara <shokohara@users.noreply.github.com>
2017-03-24 17:22:42 +0900

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -13,3 +13,8 @@ $HOME/dotfiles/deploy.sh
 # https://github.com/pyenv/pyenv/wiki/Common-build-problems#requirements
 sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+
+# need pyenv
+latestVersion=`pyenv install --list | grep -e ' [0-9]' | grep -v 'dev' | tail -n1 | tr -d ' '` ; pyenv install $latestVersion; pyenv global $_
+# reload
+pip install powerline-status
