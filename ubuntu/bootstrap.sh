b538a9d48b030a7268031dc9a94a1008689febe8
Sho Kohara <shokohara@users.noreply.github.com>
2017-03-24 17:01:41 +0900

    Update bootstrap.sh

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -8,3 +8,8 @@ yes | ~/.fzf/install
 
 git clone https://github.com/shokohara/bootstrap-ubuntu.git $HOME/dotfiles
 $HOME/dotfiles/deploy.sh
+
+# pyenv Requirements
+# https://github.com/pyenv/pyenv/wiki/Common-build-problems#requirements
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
