bd4cf7492e170b1a6c23474f39f358e2ddf5e00b
Sho Kohara <shokohara@users.noreply.github.com>
2017-06-14 23:52:10 +0900

    Update

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -1,19 +1,9 @@
 #!/bin/bash -e
 sudo apt-get update
-sudo apt-get install -y git curl
-
-sudo apt-get update
-sudo apt-get install -y zsh
-which zsh
-chsh -s $(/usr/bin/zsh)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-update-rc
+sudo apt-get install -y git curl zsh
+chsh -s $(which zsh)
 
 git clone --depth 1 https://github.com/shokohara/dotfiles.git ~/dotfiles
-git -C ~/dotfiles submodule init
-git -C ~/dotfiles submodule update
 ~/dotfiles/deploy.sh
 
 # pyenv Requirements
@@ -26,5 +16,3 @@ git clone --depth 1 https://github.com/riywo/anyenv ~/.anyenv
 source ~/.zshrc
 anyenv install pyenv
 latestVersion=`pyenv install --list | grep -e ' [0-9]' | grep -v 'dev' | tail -n1 | tr -d ' '` ; pyenv install $latestVersion; pyenv global $_
-# reload
-pip install powerline-status
