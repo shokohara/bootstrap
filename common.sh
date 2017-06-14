c12b64be291677177d36ba141b8dea53590dd0f2
Sho Kohara <shokohara@users.noreply.github.com>
2017-06-14 20:25:09 +0900

    Add git global config

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+git config --global user.email "shokohara@users.noreply.github.com"
+git config --global user.name "Sho Kohara"
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
