38d9d11623584174bbcfcf2735702b7e556806d5
shokohara <shokohara@users.noreply.github.com>
2017-07-18 15:19:27 +0900

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,6 +1,3 @@
-sudo xcodebuild -license
-xcode-select --install
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew install git
@@ -16,10 +13,6 @@ exec zsh -l
 
 ./anyenv.sh
 
-pip install --user powerline-status
-pip install --user git+git://github.com/powerline/powerline
-
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
